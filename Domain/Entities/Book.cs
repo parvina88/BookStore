7a6b19dd4ba638cf6a1ee89584f449c80d29a23f
Parvina Zulfikorova <parvina@silkroadprofessionals.com>
2024-04-05 09:08:59 +0500

    Add project files.

---
+++ Domain/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Enum;
+
+namespace Domain.Entities
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int PageCount { get; set; }
+        public BookType Type { get; set; }
+
+        public Genre Genre { get; set; }
+        public Guid GenreId { get; set; }
+
+
+
+
+    }
+}
