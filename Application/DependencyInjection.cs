a190849f9275e983225ef48483b95d75a0b4c227
Parvina Zulfikorova <parvina@silkroadprofessionals.com>
2024-04-18 11:10:12 +0500

    configure database

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Application.Services;
+using Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IBaseService<Book>, BookService>();
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+                options.UseSqlServer(connectionString);
+            });
+
+            return services;
+        }
+    }
+}
